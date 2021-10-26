e26684c46ed27c08fca3f7d47384a63c9298ed61
node-hyun <node-hyun@github.com>
2021-10-27 06:31:09 +0900

    update for ca29

--- src/main/java/jpabook/jpashop/domain/DeliveryStatus.java
+++ src/main/java/jpabook/jpashop/domain/DeliveryStatus.java
@@ -3,5 +3,5 @@ package jpabook.jpashop.domain;
 // READY: 배송중비중
 // COMP: 배송중
 public enum DeliveryStatus {
-    Ready, COMP
+    READY, COMP
 }
--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain.item;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 import javax.persistence.*;
@@ -24,5 +25,20 @@ public abstract class Item {
 
     @ManyToMany(mappedBy="items")
     private List<Category> categories = new ArrayList<>();
+
+    // 비즈니스 로직 추가
+    // stock 증가
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    // stock 감소
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock"); // NotEnoughStockException 은 커스텀 예외 클래스이므로 아래에서 별개로 생성 필요
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
